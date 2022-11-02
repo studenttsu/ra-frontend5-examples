0d6eaea32784f05a7dbf127e7bd5627defd12407
Konstantin Teryohin <konstantin.teryohin@rubius.com>
2022-11-02 19:57:51 +0700

    prettier

--- site/src/js/index.js
+++ site/src/js/index.js
@@ -6,31 +6,30 @@ import ApiService from './services/ApiService';
 
 function init() {
     $('.slider').slick({
-                        slidesToShow: 4,
-                prevArrow: '.slider-prev',
+        slidesToShow: 4,
+        prevArrow: '.slider-prev',
         nextArrow: '.slider-next',
-        infinite: false
+        infinite: false,
     });
 
     const API_PATH = 'https://beauty-saloon-server.herokuapp.com/api';
 
     const order = {
         name: 'Иван Пупкин',
-        phone: '+7 999 999-99-99'
+        phone: '+7 999 999-99-99',
     };
 
     fetch(`${API_PATH}/orders`, {
         method: 'POST',
-        body: JSON.stringify(order)
-    })
-        .then(() => {
-            console.log('Successful');
-        });
+        body: JSON.stringify(order),
+    }).then(() => {
+        console.log('Successful');
+    });
 
     async function makeRequests() {
         const { access_token } = await ApiService.login({
             userName: 'admin',
-            password: 'admin'
+            password: 'admin',
         });
 
         const orders = await ApiService.getOrders(access_token);
